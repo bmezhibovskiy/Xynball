2f4ca3cfb9abab0eeabcd7c53494705e17f7a841
Boris Mezhibovskiy <mez13526@gmail.com>
2017-03-28 22:57:11 -0700

    Added assets, walls, and paddles

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public GameObject plunger;
+    public float str = 50.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter(Collision col) { 
+        if(col.gameObject == plunger) {
+            Vector3 dir = (this.transform.position - col.gameObject.transform.position).normalized;
+            Rigidbody rigidbody = this.GetComponent<Rigidbody>();
+            rigidbody.AddForce(dir * str * rigidbody.mass, ForceMode.Force);
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+
+    public bool isRight;
+    public float range;
+    public SteamVR_TrackedController controller;
+
+    private ConfigurableJoint joint;
+
+    private void triggerDown(object sender, ClickedEventArgs e) {
+        joint.targetRotation = Quaternion.AngleAxis(range, transform.up);
+    }
+
+    private void triggerUp(object sender, ClickedEventArgs e) {        
+        joint.targetRotation = Quaternion.AngleAxis(-range, transform.up);        
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        if (isRight) {
+            range = -range;
+        }
+        
+        joint = GetComponent<ConfigurableJoint>();
+
+        joint.targetRotation = Quaternion.AngleAxis(-range, transform.up);
+
+        controller.TriggerClicked += triggerDown;
+        controller.TriggerUnclicked += triggerUp;
+    }
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+
+    
+}
